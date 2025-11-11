f73af5cb8e9c013950c161db719ddbb5c0f76088
patelvishalranjitbhai <pvishal509@gmail.com>
2025-11-11 22:48:51 +0530

    Add files via upload

---
+++ Techflex/Sources/TechFlexSecurity.xcframework/ios-arm64/TechFlexSecurity.framework/Headers/TechFlexSecurity.h
@@ -0,0 +1,18 @@
+//
+//  TechFlexSecurity.h
+//  TechFlexSecurity
+//
+//  Created by Vishal on 15/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TechFlexSecurity.
+FOUNDATION_EXPORT double TechFlexSecurityVersionNumber;
+
+//! Project version string for TechFlexSecurity.
+FOUNDATION_EXPORT const unsigned char TechFlexSecurityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TechFlexSecurity/PublicHeader.h>
+
+
